e8840086539f5c90ff8e779074b83e80d5ffe624
yamashita <yamashitaaoi1230@icloud.com>
2024-04-13 12:12:29 +0900

    add launch

---
+++ src/bezier_root/launch/bezier.launch.py
@@ -0,0 +1,18 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+def generate_launch_description():
+    web = launch_ros.actions.Node(
+            package = 'rosbridge_server',
+            executable = 'rosbridge_websocket.py',
+            parameters=[{'port': 9090}],
+            output = 'screen',
+            )
+    bezier = launch_ros.actions.Node(
+            package = 'bezier_root',
+            executable = 'bezierRoot',
+            output = 'screen',
+            )
+    return launch.LaunchDescription([web,bezier])#,serial])
